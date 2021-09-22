cf79d5f85aa7380706d49517c1571a37378acf0c
Alex Strand <alex@wpkonsulterna.se>
2021-09-22 07:01:03 +0000

    404 redirect function added to functions.php.

--- functions.php
+++ functions.php
@@ -196,3 +196,15 @@ function currentYear( $atts ){
 }
 add_shortcode( 'year', 'currentYear' );
 
+// 404 - Redirect home
+if( !function_exists('redirect_404_to_homepage') ){
+
+    add_action( 'template_redirect', 'redirect_404_to_homepage' );
+
+    function redirect_404_to_homepage(){
+       if(is_404()):
+            wp_safe_redirect( home_url('/') );
+            exit;
+        endif;
+    }
+}
