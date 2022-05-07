6b0d52a8fbd904a592c12899b6274c27b9e97d23
Alex Strand <alex@wpkonsulterna.se>
2022-05-07 12:57:07 +0200

    Update social-icons.php

--- template-parts/social-icons.php
+++ template-parts/social-icons.php
@@ -1,7 +1,7 @@
 <!-- Social icons -->
 <div class="social-icons__wrapper col-12 d-flex justify-content-center">
 	
-	<a target="_blank" href="https://www.instagram.com/alegriagrande/" title="Alegriagrande's Instagram"><i class="fab fa-instagram"></i></a>
+	<a target="_blank" href="https://www.instagram.com/alegriagrande.fit/" title="Alegriagrande's Instagram"><i class="fab fa-instagram"></i></a>
 	<a target="_blank" href="https://www.youtube.com/channel/UC7N7rozLgRsbxJwsIgvnwTw" title="Alegriagrande's YouTube Channel"><i class="fa fa-youtube"></i></a>
 	<a target="_blank" href="https://open.spotify.com/playlist/3ckiVmrJk2JopX9t6iOpxb?si=4d9cd76238434906" title="Alegriagrande's Spotify Playlist"><i class="fab fa-spotify"></i></a>
 	
